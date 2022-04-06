2df76af555b0cf1b7efc973f5bb16e12a3eea625
rajasree.r@beo.in <rajasree.r@beo.in>
2022-04-06 16:37:09 +0530

    [ADD] stock_picking_deep_sort: Added module for stock picking deep sort.

---
+++ stock_picking_deep_sort/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Stock Picking Line Deep Sort",
+    "version": "13.0.1.0.0",
+    "author": "Ametras intelligence GmbH, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Stock Management",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "summary": "Stock Picking Line Sort",
+    "depends": ["stock"],
+    "data": ["views/res_config_settings_views.xml", "views/stock_picking_views.xml"],
+    "installable": True,
+}
