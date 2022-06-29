f6dd5559304a5e875a457afb75569fc96de6cfcd
Bastian Guenther <Bastian.Guenther@ametras.com>
2022-06-29 16:07:59 +0200

    [IMP] stock_picking_mass_action: Added return statement in order to process return value

--- stock_picking_mass_action/models/stock_picking.py
+++ stock_picking_mass_action/models/stock_picking.py
@@ -14,7 +14,7 @@ class StockPicking(Model):
         """ Try to assign confirmed pickings """
         domain = [("picking_type_code", "=", "outgoing"), ("state", "=", "confirmed")]
         records = self.search(domain, order="scheduled_date")
-        records.action_assign()
+        return records.action_assign()
 
     def action_immediate_transfer_wizard(self):
         view = self.env.ref("stock.view_immediate_transfer")
