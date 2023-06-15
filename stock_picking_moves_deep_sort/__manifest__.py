bb4512d8e6a4637cd24fa04155324c0bb0cd07b4
shabeebcsbeo <shabeebcsbeo@gmail.com>
2023-06-15 15:49:59 +0530

    [FIX] stock_picking_moves_deep_sort: Fixed sorting issue for fields where numbers are character values.

--- stock_picking_moves_deep_sort/__manifest__.py
+++ stock_picking_moves_deep_sort/__manifest__.py
@@ -1,12 +1,13 @@
 {
     "name": "Stock Picking Move Deep Sort",
-    "version": "13.0.1.0.1",
+    "version": "13.0.1.0.2",
     "author": "Ametras intelligence, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Stock Management",
     "website": "https://github.com/OCA/stock-logistics-workflow",
     "summary": "Stock Picking Move Sort",
     "depends": ["stock"],
+    "external_dependencies": {"python": ["natsort"]},
     "data": [
         "views/res_config_settings_views.xml",
         "views/stock_move_views.xml",
